b486ce7679fc16757710e17de1ce9191c0395b3b
Alexander Illarionov <ruzinovrun@gmail.com>
2023-12-21 17:24:50 +0100

    add lesson 1

--- src/App.js
+++ src/App.js
@@ -18,12 +18,14 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Lesson0 from './components/Lesson0';
+import Lesson1 from './components/Lesson1';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Lesson0} />
+        <Route exact path="/lesson-1" component={Lesson1} />
       </Switch>
     </Router>
   );
