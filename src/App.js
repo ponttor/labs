e562b905543a0a25b741a94a636d4997b195bfe3
Alexander Illarionov <ruzinovrun@gmail.com>
2023-12-21 16:44:20 +0100

    add lesson 0

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Lesson0 from './components/Lesson0';
 
 function App() {
-  const style = {
-    display: 'flex',          // Задаём flex-контейнер
-    justifyContent: 'center', // Центрируем по горизонтали
-    alignItems: 'center',     // Центрируем по вертикали
-    height: '100vh',          // Высота контейнера равна 100% высоты видимой части окна браузера
-    width: '100vw',           // Ширина контейнера равна 100% ширины видимой части окна браузера
-  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={style}>
-          <iframe
-            width='660' height='420'
-            src='https://activate.infoprotector.com/cover/frame.php?video=358fe3d1-f5de-4e88-9733-8ce7d9aba4e2'
-            frameBorder='0'
-            referrerPolicy='unsafe-url'
-          />
-        </div>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Lesson0} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
