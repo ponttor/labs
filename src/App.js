0286495eec116540647a04533d4da20af0e165f8
Alexander Illarionov <ruzinovrun@gmail.com>
2023-12-21 17:21:22 +0100

    use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Lesson0 from './components/Lesson0';
+
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <Switch>
+//           <Route exact path="/" component={Lesson0} />
+//         </Switch>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Lesson0 from './components/Lesson0';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Lesson0} />
-        </Switch>
-      </Router>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Lesson0} />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
