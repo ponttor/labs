b3fa01730183bb8e4686783cd68ecd6909834b3a
Alexander Illarionov <ruzinovrun@gmail.com>
2023-12-21 13:29:12 +0100

    add video

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <iframe
+          width='660' height='420'
+          src='https://activate.infoprotector.com/cover/frame.php?video=358fe3d1-f5de-4e88-9733-8ce7d9aba4e2'
+          frameBorder='0'
+          referrerPolicy='unsafe-url'
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
