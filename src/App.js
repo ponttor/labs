399bed32c52d64c250122704120aec5d5f4f4e93
Alexander Illarionov <ruzinovrun@gmail.com>
2023-12-22 14:47:04 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Lesson0 from './components/Lesson0';
 import Lesson1 from './components/Lesson1';
-import Lesson1 from './components/Lesson2';
-import Lesson1 from './components/Lesson3';
-import Lesson1 from './components/Lesson4';
-import Lesson1 from './components/Lesson5';
-import Lesson1 from './components/Lesson6';
-import Lesson1 from './components/Lesson7';
-import Lesson1 from './components/Lesson8';
-import Lesson1 from './components/Lesson9';
-import Lesson1 from './components/Lesson10';
-import Lesson1 from './components/Lesson11';
-import Lesson1 from './components/Lesson12';
-import Lesson1 from './components/Lesson13';
-import Lesson1 from './components/Lesson14';
+import Lesson2 from './components/Lesson2';
+import Lesson3 from './components/Lesson3';
+import Lesson4 from './components/Lesson4';
+import Lesson5 from './components/Lesson5';
+import Lesson6 from './components/Lesson6';
+import Lesson7 from './components/Lesson7';
+import Lesson8 from './components/Lesson8';
+import Lesson9 from './components/Lesson9';
+import Lesson10 from './components/Lesson10';
+import Lesson11 from './components/Lesson11';
+import Lesson12 from './components/Lesson12';
+import Lesson13 from './components/Lesson13';
+import Lesson14 from './components/Lesson14';
 
 function App() {
   return (
